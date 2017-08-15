230825e4439c670e5105b5c6a34d4c2575898f0c
sean_codes <sean.mann@alpinehomeair.com>
2017-08-15 16:38:07 -0500

    created

---
+++ README.md
@@ -0,0 +1,3 @@
+# Atom Windows Titlebar
+
+The titlebar replacement for Atom on Windows
---
+++ spec/atom-windows-titlebar-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomWindowsTitlebarView from '../lib/atom-windows-titlebar-view';
+
+describe('AtomWindowsTitlebarView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
